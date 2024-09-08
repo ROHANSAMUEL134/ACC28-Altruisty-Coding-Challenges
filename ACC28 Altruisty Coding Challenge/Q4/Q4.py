7975ea024219e3126054b905ef9d842bf3e0bac3
ROHAN SAMUEL J <rohansamuel134@gmail.com>
2024-09-08 18:49:26 +0530

    Update Q4.py

--- ACC28 Altruisty Coding Challenge/Q4/Q4.py
+++ ACC28 Altruisty Coding Challenge/Q4/Q4.py
@@ -3,23 +3,22 @@ you have mentioned answer should not be zero'''
 #But the output you provided for the sample input zero
 #So let me provide answer for both
 
-#As per the expected output
+
 s="203"
 k=2
 removed=0
 s=list(s)
-temp=sorted(s,reverse=1)
+
+#As per the expected output
 for i in range(k):
-    s.remove(temp[i])
+    s.remove(s[i])
 print("If answer can be Zero:",''.join(s))
+
 
 #As per the problem statement i.e answer should not be zero
 s="203"
-k=2
-removed=0
 s=list(s)
 num_cnt=len(s)-s.count('0')
-temp=sorted(s,reverse=1)
 if num_cnt>k:
     for i in range(k):
         s.remove(temp[i])
@@ -28,10 +27,10 @@ else:
     diff=num_cnt
     i=0
     while num_cnt:
-        s.remove(temp[i])
+        s.remove(s[i])
         i+=1
         num_cnt-=1
     for i in range(diff):
         s.remove('0')
     
-print("If answer can't be Zero:",''.join(s))
+print("If answer can't be Zero:",int(''.join(s)))
